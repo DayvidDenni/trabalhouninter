32d23b932cbd346744e0160ce3feae3467b7c267
DayvidDenni <dayvid_prados@hotmail.com>
2025-05-17 10:34:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let botao = document.querySelector("#botao");
+
+botao.addEventListener("click",clique);
+function clique() {
+   let mensagem = document.querySelector("#mensagem");
+   mensagem.innerHTML="Comece de onde você está,use o que você tem,faça o que você pode.Acredite em você!";
+    
+};
